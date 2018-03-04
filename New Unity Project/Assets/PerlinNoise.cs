e06c214e1ae6dd9d049faab6e6ec18ec64a66fdd
Phillip <phillipables1@gmail.com>
2018-03-04 13:26:50 -0500

    loop through each y pixel of the x pixel, Calculate color using x and y percent coordinates passed through a MAthf perflinnoise function and return a new color using those values for r g b, then apply texture and return texture

--- New Unity Project/Assets/PerlinNoise.cs
+++ New Unity Project/Assets/PerlinNoise.cs
@@ -11,5 +11,27 @@ public class PerlinNoise : MonoBehaviour {
         renderer.material.mainTexture = GenerateTexture();
     }
 
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height); //creating a texture from scratch so we need to create a texture variable
+        //generate a perlin noise map for the texture
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        return texture;
+    }
 
+    Color CalculateColor (int x, int y)
+    {
+        float xCoord = x / width;
+        float yCoord = y / height;
+
+        float smaple = Mathf.PerflinNoise(x, y);
+        return new Color(sample, sample, sample);
+    }
 }
