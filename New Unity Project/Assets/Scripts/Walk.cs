1b605cd99d60636f2dad6d6a5682ea8aefcbc6b3
Phillip <phillipables1@gmail.com>
2018-03-05 20:58:48 -0500

    animation script

---
+++ New Unity Project/Assets/Scripts/Walk.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : MonoBehaviour {
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Movement();
+        {
+            float move = Input.GetAxis("Horizontal");
+            amin.SetFloat("Speed", move);
+        }
+    }
+
+    void Movement()
+    {
+        if(Input.GetKey (KeyCode.D))
+        {
+            transform.Translate(Vector2.right = 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if(Input.GetKey (KeyCode.A))
+        {
+            tranform.Translate(Vector.right = 3f * Time.deltaTime);
+            transform.eulerAngles = new Vector(0, -180);
+        }
+    }
+}
